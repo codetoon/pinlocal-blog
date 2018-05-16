8961dd02f130655bea110a09efe3966da8de6958
nilam patel <nilam.patel@regur.net>
2018-05-16 19:15:45 +0530

    added buttons in header

--- wp-content/themes/simplelin-child/header.php
+++ wp-content/themes/simplelin-child/header.php
@@ -27,14 +27,26 @@
 	<header id="masthead" class="site-header" role="banner">
 
 		<div class="container">
-
+		
 			<div class="site-branding">
+
+				<?php if ( has_custom_logo() ) : ?>
+					<div class="site-logo">
+						<?php the_custom_logo(); ?>
+					</div><!-- .site-logo -->
+				<?php endif; ?>
+
 				<div class="title-area">
 					<?php
 						if ( is_front_page() && is_home() ) : ?>
-							<h1 class="site-title"><a href="http://pinlocal.com/" rel="home"><img src="<?php echo esc_url( home_url( '/' ) );?>wp-content/themes/simplelin-child/images/pinlocal_logo.png" alt="images"></a></h1>
-					
-					
+							<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+						<?php else : ?>
+							<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+					<?php endif;
+
+					$description = get_bloginfo( 'description', 'display' );
+					if ( $description || is_customize_preview() ) : ?>
+						<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
 					<?php endif; ?>
 				</div><!-- .title-area -->
 				
@@ -46,28 +58,6 @@
 			</div><!-- .site-branding -->
 
 		</div><!-- .container -->
-
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-
-			<?php if ( has_nav_menu( 'primary') ) { ?>
-				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Menu', 'simplelin' ); ?></button>
-				<?php
-					wp_nav_menu( array(
-						'container'      => '',
-						'theme_location' => 'primary',
-						'menu_id'        => 'primary-menu',
-					) );
-			} else { ?>
-					
-				<ul id="primary-menu" class="sf-menu">
-					<?php if ( current_user_can( 'edit_theme_options' ) ) { ?>
-						<li class="menu-item"><?php printf( __( '<a href="%s">Add menu</a>', 'simplelin' ), esc_url( admin_url( 'nav-menus.php' ) ) ); ?></li>
-					<?php } ?>
-				</ul><!-- #primary-menu -->
-
-			<?php } ?>
-
-		</nav><!-- #site-navigation -->
 
 	</header><!-- #masthead -->
 			
